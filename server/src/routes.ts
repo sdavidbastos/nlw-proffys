1cb559dfc13dd6a2da6780f6cc64e68f7dcae138
david <davidtavares98@gmail.com>
2020-08-05 15:15:35 -0300

    API Finalizada

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,60 +1,16 @@
 import express from "express";
-import db from "./database/connection";
-import convertHourToMinutes from "./utils/convertHourToMinutes";
+import ClassesController from "./controllers/ClassesController";
+import ConnectionsController from "./controllers/ConnectionsController";
 
 const routes = express.Router();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
+const classesController = new ClassesController();
+const connectionsController = new ConnectionsController();
 
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
+routes.post("/classes", classesController.create);
+routes.get("/classes", classesController.index);
 
-  const trx = await db.transaction();
-
-  try {
-    // função retorna o id do usuarios inseridos
-    const insertedUsersIds = await trx("users").insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await trx("classes").insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        class_id,
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-      };
-    });
-
-    await trx("class_schedule").insert(classSchedule);
-
-    await trx.commit();
-
-    return response.status(201).send();
-  } catch (error) {
-    await trx.rollback();
-
-    console.log(error);
-    return response.status(400).json({
-      error: "Unexpected error while create new class",
-    });
-  }
-});
+routes.post("/connections", connectionsController.create)
+routes.get("/connections", connectionsController.index)
 
 export default routes;
